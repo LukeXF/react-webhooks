ffbafae62bfd9ad9c4ea08dfe72f4b1a807e7e77
Luke Brown <me@luke.sx>
2020-08-01 03:07:07 +0100

    Added button to mimic pageView event

--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 import io from 'socket.io-client';
 
+const socket = io('ws://localhost:3001');
+
 function App() {
 	// const socket = io();
 	console.log("socket");
-	const socket = io('ws://localhost:3001');
 
 	console.log({socket});
-
-	socket.on('connect', () => {
-		// either with send()
-		socket.send('Hello!');
-
-		// or with emit() and custom event names
-		socket.emit('salutations', 'Hello!', {'mr': 'john'}, Uint8Array.from([1, 2, 3, 4]));
-	});
+	//
+	// socket.on('connect', () => {
+	// 	// either with send()
+	// 	socket.send('Hello!');
+	//
+	// 	// or with emit() and custom event names
+	// 	socket.emit('event', 'Hello! v3', {'mr': 'john'}, Uint8Array.from([1, 2, 3, 4]));
+	// });
 
 	socket.on("connect", () => {
+		console.log('on connect', socket);
 		if (socket) {
+			console.log('trying to authenticate');
 			socket.emit("authenticate", {
-				token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlMzZjYWUxYTYxZGE5MTQ3ZGU5NGM2MiIsImVtYWlsIjoibHVrZUBlbGVtZW50c29mdHdvcmtzLmNvLnVrIiwiaWF0IjoxNTk2MjQwNDIxLCJleHAiOjE1OTYyNDIyMjF9.-J39hVsRkNCZ5mxA7lmIH6ZBmSLyIytdjXQqTKU3OOU'
+				token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlMzZjYWUxYTYxZGE5MTQ3ZGU5NGM2MiIsImVtYWlsIjoibHVrZUBlbGVtZW50c29mdHdvcmtzLmNvLnVrIiwiaWF0IjoxNTk2MjQ3MzMyLCJleHAiOjE1OTYyNDkxMzJ9.w3RDgDwx4KeqGoxW5_vSqXwE063B-1ixOhUXg1Vg0rY'
 			}); //send the jwt
 		}
 	});
@@ -30,12 +33,12 @@ function App() {
 		console.log(data.body);
 	});
 
-// handle the event sent with socket.send()
+	// handle the event sent with socket.send()
 	socket.on('message', data => {
 		console.log(data);
 	});
 
-// handle the event sent with socket.emit()
+	// handle the event sent with socket.emit()
 	socket.on('greetings', (elem1, elem2, elem3) => {
 		console.log(elem1, elem2, elem3);
 	});
@@ -46,17 +49,18 @@ function App() {
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
+				<button onClick={ActionLink}>
+					Activate Lasers
+				</button>
 			</header>
 		</div>
 	);
+}
+
+function ActionLink(e) {
+	e.preventDefault();
+	console.log('The link was clicked.');
+	socket.emit('test', 'Hello! v3', {'mr': 'john'},);
 }
 
 export default App;
